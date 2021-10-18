0dfe286fda1c90cb778362c3b80f264035a04579
nopibal <ytneirby1@gmail.com>
2021-10-18 10:54:26 +0300

    add authentification, login logout sign up

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+
+def home(request):
+    return render(request, 'todo/home.html')
 
 def signupuser(request):
     if request.method == 'GET':
@@ -18,6 +21,24 @@ def signupuser(request):
                 return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'That username has already taken. Please choose a new one'})
         else:
             return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form':AuthenticationForm(),'error':'Username and password did not match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
